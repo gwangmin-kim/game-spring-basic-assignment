8a12557ba9377b7d95704d3c44c25b7967277384
gwangmin-kim <gwangmin@snu.ac.kr>
2026-09-09 13:07:44 +0900

    fix: level8: 검증 조건 정정

--- src/main/java/com/gamebasic/game/dto/RenameRequest.java
+++ src/main/java/com/gamebasic/game/dto/RenameRequest.java
@@ -1,12 +1,12 @@
 package com.gamebasic.game.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class RenameRequest {
-    @NotNull
+    @NotBlank
     @Size(min = 2, max = 12)
     private String playerName;
 }

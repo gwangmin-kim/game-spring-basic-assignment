2b85e74b61e9e10335d471ae72876697892e54e8
gwangmin-kim <gwangmin@snu.ac.kr>
2026-09-09 11:33:20 +0900

    level 7: JPA Auditing 준비

--- src/main/java/com/gamebasic/game/entity/Game.java
+++ src/main/java/com/gamebasic/game/entity/Game.java
@@ -1,5 +1,6 @@
 package com.gamebasic.game.entity;
 
+import com.gamebasic.common.entity.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "games")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Game {
+public class Game extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
